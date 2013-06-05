9e17e0e6e0ccc800aa2a25a042b0abe1a4698d7c
Frank Calise <fcalise@gmail.com>
2013-06-05 16:00:15 -0400

    ViewPager set up

--- Hydrate/build/source/r/debug/com/frankcalise/h2droid/R.java
+++ Hydrate/build/source/r/debug/com/frankcalise/h2droid/R.java
@@ -18,10 +18,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080002;
+        public static final int fragment_textview=0x7f080001;
+        public static final int pager=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int fragment_main=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -29,7 +32,8 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050000;
         public static final int app_name=0x7f050001;
-        public static final int hello_world=0x7f050002;
+        public static final int tab_history_name=0x7f050002;
+        public static final int tab_track_name=0x7f050003;
     }
     public static final class style {
         /** 
--- Hydrate/src/main/java/com/frankcalise/h2droid/MainActivity.java
+++ Hydrate/src/main/java/com/frankcalise/h2droid/MainActivity.java
@@ -1,17 +1,56 @@
 package com.frankcalise.h2droid;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import android.app.ActionBar;
 
-public class MainActivity extends Activity {
+import android.app.FragmentTransaction;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class MainActivity extends FragmentActivity implements ActionBar.TabListener {
+
+    MainFragmentPagerAdapter mMainFragmentPagerAdapter;
+    ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        mMainFragmentPagerAdapter = new MainFragmentPagerAdapter(getSupportFragmentManager(),
+                                                                 this);
+
+        final ActionBar actionBar = getActionBar();
+
+        actionBar.setDisplayHomeAsUpEnabled(false);
+
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mMainFragmentPagerAdapter);
+        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                actionBar.setSelectedNavigationItem(position);
+            }
+        });
+
+        for (int i = 0; i < mMainFragmentPagerAdapter.getCount(); i++) {
+            actionBar.addTab(
+                    actionBar.newTab()
+                        .setText(mMainFragmentPagerAdapter.getPageTitle(i))
+                        .setTabListener(this));
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -19,5 +58,72 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+
+    }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+        mViewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+
+    }
+
+    public static class MainFragmentPagerAdapter extends FragmentPagerAdapter {
+
+        private Context mContext;
+
+        public MainFragmentPagerAdapter(FragmentManager fragmentManager, Context context) {
+            super(fragmentManager);
+            mContext = context;
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            switch(i) {
+                default:
+                    Fragment fragment = new SectionFragment();
+                    Bundle args = new Bundle();
+                    args.putInt(SectionFragment.ARG_SECTION_NUMBER, i + 1);
+                    fragment.setArguments(args);
+                    return fragment;
+
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            if (position == 0) {
+                return mContext.getString(R.string.tab_track_name);
+            } else {
+                return mContext.getString(R.string.tab_history_name);
+            }
+        }
+    }
+
+    public static class SectionFragment extends Fragment {
+
+        public static final String ARG_SECTION_NUMBER = "section_number";
+
+        @Override
+        public View onCreateView(LayoutInflater inflater,
+                                 ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            Bundle args = getArguments();
+            ((TextView) rootView.findViewById(R.id.fragment_textview)).setText(
+                    getString(R.string.app_name));
+            return rootView;
+        }
+    }
 }
